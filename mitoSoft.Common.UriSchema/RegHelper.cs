0d16222b9c6c8b948f11b5d57c6f0a3acd27da9d
Michael <michael.mr.roth@live.com>
2022-04-02 11:48:58 +0200

    initial

---
+++ mitoSoft.Common.UriSchema/RegHelper.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace mitoSoft.Common.UriSchema
+{
+    public class RegistryHelper
+    {
+        public static void RegisterScheme(string uriSchema, string applicationPath)
+        {
+            using var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Classes\\" + uriSchema);
+
+            key.SetValue("", "URL:" + $"{uriSchema} Protocol"); //FriendlyName
+            key.SetValue("URL Protocol", "");
+
+            using (var defaultIcon = key.CreateSubKey("DefaultIcon"))
+            {
+                defaultIcon.SetValue("", applicationPath + ",1");
+            }
+
+            using var commandKey = key.CreateSubKey(@"shell\open\command");
+            commandKey.SetValue("", "\"" + applicationPath + "\" \"%1\"");
+        }
+
+        public static string? IsRegistered(string uriSchema)
+        {
+            using var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Classes\\" + uriSchema);
+
+            if (key == null)
+            {
+                return null;
+            }
+
+            using var commandKey = key.CreateSubKey(@"shell\open\command");
+            if (commandKey.ValueCount <= 0)
+            {
+                return null;
+            }
+
+            var value = commandKey.GetValue("");
+            
+            string path = ParseValueToPath(value);
+
+            return path;
+        }
+
+        private static string ParseValueToPath(object? value)
+        {
+            try
+            {
+                if (value == null)
+                {
+                    throw new Exception("No value to parse");
+                }
+                var path = value.ToString();
+                path = path.Replace("\"", "");
+                path = path.Replace(" %1", "");
+                return path;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Value '{value?.ToString()}' not possible to parse: {ex.Message}");
+            }
+        }
+    }
+}
